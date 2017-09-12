d19d90e61e756b50cbb8c792555ede4205e42142
Graham Hughes <graham_hughes@berkeley.edu>
2017-09-12 11:39:32 -0700

    Finished ex2 fibonacci contract

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		uint a = 0;
+		uint b = 1;
+		for (uint i = 0; i < position; i++) {
+		    uint temp = b;
+		    b = b + a;
+		    a = temp;
+		}
+		return b;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+    function () {}
 }
